0ac6b200e5c40b4f0ca5f3d103757d99c145195d
mert.yagci <mert.yagci@trendyol.com>
2025-03-08 22:51:38 +0300

    bug fixes

--- Wellness Ai/Views/Chat/ChatBubble.swift
+++ Wellness Ai/Views/Chat/ChatBubble.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import UIKit
 
 struct ChatBubble: Shape {
     let isFromUser: Bool
@@ -6,12 +7,9 @@ struct ChatBubble: Shape {
     func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(
             roundedRect: rect,
-            byRoundingCorners: [
-                .topLeft: !isFromUser,
-                .topRight: true,
-                .bottomLeft: true,
-                .bottomRight: isFromUser
-            ],
+            byRoundingCorners: isFromUser
+                ? [.topLeft, .topRight, .bottomLeft]
+                : [.topRight, .bottomLeft, .bottomRight],
             cornerRadii: CGSize(width: 15, height: 15)
         )
 
@@ -21,18 +19,9 @@ struct ChatBubble: Shape {
 
 struct ChatBubble_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ChatBubble(isFromUser: true)
-                .frame(width: 200, height: 100)
-                .previewLayout(.sizeThatFits)
-                .padding()
-                .foregroundColor(.blue)
-
-            ChatBubble(isFromUser: false)
-                .frame(width: 200, height: 100)
-                .previewLayout(.sizeThatFits)
-                .padding()
-                .foregroundColor(.gray)
-        }
+        ChatBubble(isFromUser: true)
+            .frame(width: 200, height: 50)
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
